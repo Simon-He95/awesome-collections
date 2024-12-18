df14d54c622cca8cce5376ca9727ce54b1bca9cd
Simon He <57086651+Simon-He95@users.noreply.github.com>
2024-12-19 00:06:39 +0800

    chore: update deps

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+const antfu = require('@antfu/eslint-config').default
+
+module.exports = antfu(
+  {
+    ignores: [
+      // eslint ignore globs here
+      'test/**/*',
+    ],
+  },
+  {
+    rules: {
+      // overrides
+    },
+  },
+)
